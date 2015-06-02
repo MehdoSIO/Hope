abe45e4715be119605b4b15c4c3c66ab6c08b44b
MehdoSIO <mehdi.manouni@hotmail.fr>
2015-06-02 20:33:48 +0200

    deploiement du projet le 02/06/2015  Version 0.0.0

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php 
-        echo " je suis un génie"
+        echo "je suis une andouille"
         ?>
     </body>
 </html>
